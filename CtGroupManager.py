50f8deb3eafb0443002708439126590dbdc3780b
Lukas Block <lukas@blockmail.de>
2025-08-24 15:08:44 +0000

    Added AbstractCtManager

--- CtGroupManager.py
+++ CtGroupManager.py
@@ -1,20 +1,8 @@
 import requests
 
-class CtGroupManager:
-    def __init__(self, server_url, api_user, api_pwd, timeout):
-        self.server_url = server_url.rstrip('/')
-        self.api_user = api_user
-        self.api_pwd = api_pwd
-        self.timeout = timeout
+from AbstractCtManager import AbstractCtManager
 
-        self.session = requests.Session()
-
-    def login(self):
-        r = self.session.post(f"{self.server_url}/api/login", json={
-            "username": self.api_user,
-            "password": self.api_pwd
-        }, timeout=self.timeout)
-        r.raise_for_status()
+class CtGroupManager(AbstractCtManager):
 
     def get_user_groups(self, person_id):
         if not isinstance(person_id, int):
