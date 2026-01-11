59404aee360f60e74ece3e31d33129bac22241e2
Lukas B. <61112699+lub90@users.noreply.github.com>
2026-01-11 17:31:35 +0100

    Squashed 'extension/' content from commit c723b01
    git-subtree-dir: extension
    git-subtree-split: c723b0154f751412eced661e9c2384f4f5632775

---
+++ vite.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig, loadEnv } from 'vite';
+
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+    return defineConfig({
+        base: `/ccm/${process.env.VITE_KEY}/`,
+    });
+};
