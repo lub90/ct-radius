6e12791a760cccf2b6117ab1aa74db559c9c29f1
lukas@blockmail.de <Lukas Block>
2026-01-13 22:04:05 +0100

    Basic setup of extension finished

---
+++ extension/src/layouts/Root.vue
@@ -0,0 +1,31 @@
+<template>
+  <v-layout height="100%">
+    <v-main class="h-screen bg-background">
+        <v-container fluid class="pa-5 h-screen overflow-y-auto">
+            <v-card elevation="1">
+            <!-- title -->
+            <v-card-title>
+                <slot name="title" />
+            </v-card-title>
+
+            <v-card-subtitle>
+                <slot name="subtitle" />
+            </v-card-subtitle>
+
+            <!-- main content -->
+            <v-card-text>
+                <slot />
+            </v-card-text>
+
+            <!-- footer -->
+            <v-card-actions>
+                <slot name="footer" />
+            </v-card-actions>
+            </v-card>
+        </v-container>
+    </v-main>
+  </v-layout>
+</template>
+
+<script setup lang="ts">
+</script>
--- extension/vite.config.ts
+++ extension/vite.config.ts
@@ -1,9 +1,23 @@
 import { defineConfig, loadEnv } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vuetify, { transformAssetUrls } from 'vite-plugin-vuetify';
+import path from "path";
 
 // https://vitejs.dev/config/
 export default ({ mode }) => {
     process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
     return defineConfig({
         base: `/ccm/${process.env.VITE_KEY}/`,
+        plugins: [
+            vue({
+                template: { transformAssetUrls }
+            }),
+            vuetify({ autoImport: true }) 
+        ],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "src")
+            }
+        }
     });
 };
